98f78eee2e63e8a7a60b074d7a8544b3cadc7e24
Anhang Zhu <anhang@visor.gg>
2019-02-19 13:58:59 -0800

    add iohook

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const {app, BrowserWindow} = require('electron');
 
 let mainWindow;
 
+const ioHook = require("iohook");
+ioHook.on("keydown", (evt) => {
+  console.log(evt);
+});
+
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
       height: 600,
       width: 800
   });
-
   mainWindow.webContents.openDevTools({mode: "detach"});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  ioHook.start();
 });
